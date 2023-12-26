5a60ae4ae6d7193a78fa4c233cf587feb8d983d1
Francesco <todinofrancesco97@gmail.com>
2023-12-26 13:54:57 +0100

    sequential distance

--- main.cpp
+++ main.cpp
@@ -1,7 +1,59 @@
 #include <iostream>
-
+#include <chrono>
 using namespace std;
+using namespace chrono;
 
-int main() {
-   cout << "Init";
+string generateWord(int n)
+{
+   string word = "";
+   for (int i = 0; i < n; i++)
+   {
+      word += (char)('a' + rand() % 26);
+   }
+   return word;
+}
+
+int sequentialDistance(string A, string B)
+{
+   unsigned int lenA = A.size();
+   unsigned int lenB = B.size();
+
+   unsigned int **D = new unsigned int *[lenA + 1];
+   for (int i = 0; i < lenA + 1; i++)
+      D[i] = new unsigned int[lenB + 1];
+
+   for (int i = 0; i < lenA + 1; i++)
+      D[i][0] = i;
+   for (int j = 1; j < lenB + 1; j++)
+      D[0][j] = j;
+
+   for (int i = 1; i < lenA + 1; i++)
+   {
+      for (int j = 1; j < lenB + 1; j++)
+      {
+         if (A[i - 1] == B[j - 1])
+         {
+            D[i][j] = D[i - 1][j - 1];
+         }
+         else
+         {
+            D[i][j] = 1 + min(min(D[i - 1][j], D[i][j - 1]), D[i - 1][j - 1]);
+         }
+      }
+   }
+
+   return D[lenA][lenB];
+}
+
+int main()
+{
+   string A = generateWord(10000);
+   string B = generateWord(10000);
+
+   // SEQUENTIAL
+   auto start = system_clock::now();
+   int distance = sequentialDistance(A, B);
+   auto end = system_clock::now();
+   auto elapsed = duration_cast<milliseconds>(end - start);
+   cout << "seq Distance (n=" << A.size() << ", d=" << distance << "): " << elapsed.count() << "ms" << endl;
 }
